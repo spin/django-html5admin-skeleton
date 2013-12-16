bc75e8aee8f2397c316f57c80e876c009bd38b38
Spin Lai <Pengo.Lai@gmail.com>
2013-12-16 19:58:48 +0800

    Add `demo.urls` to `project_name.urls`

--- project_name/urls.py
+++ project_name/urls.py
@@ -7,6 +7,6 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', '{{ project_name }}.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^{{ project_name }}/demo/$', include('demo.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
