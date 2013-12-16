6ea6e39fdca659fd38e3a82e4378e78de22d1310
Spin Lai <Pengo.Lai@gmail.com>
2013-12-16 20:12:36 +0800

    Add project name as the prefix of the default DB file name.

--- project_name/settings/base.py
+++ project_name/settings/base.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'south',
+    'demo',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -59,7 +60,7 @@ WSGI_APPLICATION = '{{ project_name }}.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, '{{ project_name }}_db.sqlite3'),
     }
 }
 
