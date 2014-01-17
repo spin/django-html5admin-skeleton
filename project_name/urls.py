12105566b6d61446bdf907776176dd630bf41407
Spin Lai <Pengo.Lai@gmail.com>
2014-01-17 21:36:19 +0800

    Dispatch root url to demo view.

--- project_name/urls.py
+++ project_name/urls.py
@@ -7,6 +7,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', '{{ project_name }}.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+    url(r'^{{ project_name }}/$', include('demo.urls')),
     url(r'^{{ project_name }}/demo/', include('demo.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
