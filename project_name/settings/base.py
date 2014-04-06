16fbbc3fc26e735afccad591c1967b34d9059f70
Spin Lai <Spin.Lai@audatex.com>
2014-04-06 23:16:11 +0800

    Remove SECRET_KEY in `settings.base`.

--- project_name/settings/base.py
+++ project_name/settings/base.py
@@ -16,8 +16,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'd&3ppxjx_k6tpro(u36yy-35up+sr=+x14ch22d1+@2k1c_tid'
+# Check this : https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
+SECRET_KEY = r"{{ secret_key }}"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
